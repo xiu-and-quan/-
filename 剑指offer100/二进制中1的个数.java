333ff266ff476bbffb9bdfeb9789afe00e94b864
shizq18 <shizq18@meicloud.com>
2023-03-30 15:57:38 +0800

    序列化和反序列化不能用基础类型

--- 剑指offer100/二进制中1的个数.java
+++ 剑指offer100/二进制中1的个数.java
@@ -4,27 +4,31 @@ import java.util.Arrays;
 
 public class 二进制中1的个数 {
     public static void main(String[] args) {
-        //System.out.println(hammingWeight(-3));
+        System.out.println(hammingWeight(-3));
         //左移和右移
         /*int res = (1 << 0);
         System.out.println(res);*/
         /*String res = Integer.toBinaryString(-3);
         System.out.println(res);
         System.out.println(Integer.toBinaryString(0));*/
-        System.out.println(1>>1);
-        System.out.println(1<<1);
-        System.out.println(1<<0);
+
     }
     public static int hammingWeight(int n) {
         int resCount = 0;
-        while (n != 0){
+        //位运算
+        for (int i = 0; i < 32; i++) {
+            if ((n & (1 << i)) != 0){
+                resCount++;
+            }
+        }
+       /* while (n != 0){
             if (n % 2 == 1){
                 resCount++;
                 n = n-1;
             }else {
                 n = n/2;
             }
-        }
-        return resCount++;
+        }*/
+        return resCount;
     }
 }
---
+++ 剑指offer100/数值的整数次方.java
@@ -0,0 +1,15 @@
+package 剑指offer100;
+
+public class 数值的整数次方 {
+    public static void main(String[] args) {
+        //System.out.println(myPow(2,-2));
+        double res =Math.pow(0.00001,2147483647);
+        System.out.println(res);
+    }
+    public static double myPow(double x, int n) {
+        if (n == 0){
+            return 1.0;
+        }
+        return 1.0;
+    }
+}
